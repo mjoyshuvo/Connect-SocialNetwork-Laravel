93fe2c9e31fe11aa7e659bc4be213c9039043eaf
mjoyshuvo <mjoyshuvo@gmail.com>
2016-08-08 01:14:26 +0600

    Create User Profile

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,44 +1,32 @@
 <?php
-
-
 Route::get('/', [
-
 	'uses' => 'HomeController@index',
 	'as' => 'home'
-
 	]);
 /**
  * Sign in and Signup
  */
 Route::get('/signup', [
-
 	'uses' => 'AuthController@getSignup',
 	'as' => 'signup',
 	'middleware' => ['guest'],
 	]);
-
 Route::post('/signup',[
 	'uses'=>'AuthController@postSignup',
 	'middleware' => ['guest'],
 	]);
-
 Route::get('/signin', [
-
 	'uses' => 'AuthController@getSignin',
 	'as' => 'signin',
 	'middleware' => ['guest'],
 	]);
-
 Route::post('/signin',[
 	'uses'=>'AuthController@postSignin',
 	'middleware' => ['guest'],
 	]);
-
 Route::get('/signout', [
-
 	'uses' => 'AuthController@getSignout',
 	'as' => 'signout'
-
 	]);
 /**
  * Search Route
@@ -46,4 +34,12 @@ Route::get('/signout', [
 Route::get('/search',[
 	'uses'=> 'SearchController@getResults',
 	'as' => 'search.result'
+	]);
+
+/**
+ * User Profile
+ */
+Route::get('/user/{username}',[
+	'uses'=> 'ProfileController@getProfile',
+	'as' => 'profile.index'
 	]);
